9aaad253b26e24b3e22d33b3bbaf9256874a638e
Gemeng Qin <gqin@thoughtworks.com>
2019-10-08 17:50:45 +0800

    Update package config

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Testlib.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
     ]
   end
 
@@ -18,20 +20,27 @@ defmodule Testlib.MixProject do
     ]
   end
 
+  defp description do
+    """
+    test helloworld lib
+    """
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README", "LICENSE*"],
+      files: ["lib", "mix.exs", "README.md"],
       maintainers: ["Gemeng"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/fteem/eight_ball"}
+      links: %{"GitHub" => "https://github.com/qingemeng/testlib"}
     ]
   end
 end
